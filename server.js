1724b807d6a9eefa8ae2870b5414b4d372aebd4a
YVE DE SOUZA ARAUJO <24153572@edu119.local>
2025-06-05 09:30:40 -0300

    adicionando disciplinas e professores ok

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Rotas
 app.use('/clientes', require('./routes/cliente'));
-app.use('/usuarios', require('./routes/usuario')); // <-- nova rota adicionada aqui
+app.use('/usuarios', require('./routes/usuario')); 
+app.use('/disciplinas', require('./routes/disciplina'));
+app.use('/professores', require('./routes/professor')); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API rodando em http://localhost:${PORT}`));
